cb74d89262b6b16e681754120c5c7ba1c6f1df20
sunsunnp <sunsun.np@gmail.com>
2018-10-18 11:41:51 +0700

    add file Hello

--- src/Hello.java
+++ src/Hello.java
@@ -1,4 +1,17 @@
 
 public class Hello {
-
+	String name;
+	public Hello(String name) {
+		this.name = name;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getHello() {
+		String Hello = "Hello" + this.name;
+		return Hello;
+	}
 }
